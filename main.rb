c6373a8f8cb5e4a8a9f776e306f854229e332867
young13kkk <111087415+young13kkk@users.noreply.github.com>
2022-08-12 12:33:06 -0600

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,55 @@
+puts "Hello, World!"
+  
+puts "H1" 'eeeeee'
+
+myFirstVariable = 'aaa'
+
+puts myFirstVariable
+
+moduloAnswer=5%2 #moduloAnswer=1
+exponentAnswer=2**3 #exponentAnswer=8
+
+"H1" length 
+
+**is used for exponents; 5**2 is 5 to the power of 2, which is 25
+
+
+
+#=aString = "I'm a string!" aBoolean = true aNumber = 42
+
+=begin
+I'm a comment!
+=end
+
+
+#puts "Hi!" #I'm a comment, but Hi! will still be printed to the console.
+
+
+% is a little weird. In Ruby, it isn't percent, it is modulo. Modulo is like division, but it gives you just the remainder of a division problem.
+
+
+numberOne = 6
+numberTwo = 8
+numberThree = 5
+numberFour = 12
+numberFive = 36
+
+testOne = numberTwo * numberThree #fill in the blank to make this 40
+testTwo = numberFive % numberOne #fill in the blank to make this 0
+testThree = numberFour - numberThree #fill in the blank to make this 7
+if testOne == 40 && testTwo == 0 && testThree == 7
+    puts true
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
